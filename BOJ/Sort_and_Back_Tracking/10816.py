ef53cf969709f220d751cf4b07d91e2439882f43
1stone <qkatorxhrl@gmail.com>
2022-02-09 23:01:49 +0900

    feat : BOJ sort

--- BOJ/Sort_and_Back_Tracking/10816.py
+++ BOJ/Sort_and_Back_Tracking/10816.py
@@ -4,6 +4,8 @@
 숫자 카드 N개를 갖고, 정수 M개가 주어졌을 때, 
 이 수가 적혀있는 숫자 카드를 몇 개 가지고 있는지 구하는 프로그램 작성
 '''
+# 10815와 동일
+
 from collections import Counter
 import sys
 input = sys.stdin.readline
@@ -20,3 +22,26 @@ for i in m_list:
         print(n_count[i], end=' ')
     else:
         print(0, end=' ')
+
+'''
+# 맞힌사람 코드
+import sys
+
+def test():
+    
+    input()
+    b = set(input().split()) # 내가 가진 값
+    input()
+    d = input().split()      # 찾아야 하는 값
+    
+    res = ''
+    for i in d:
+        if i in b:
+            res += '1 '
+        else:
+            res += '0 '
+    
+    print(res)
+
+test()
+'''
